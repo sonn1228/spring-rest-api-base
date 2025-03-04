dcafefd949adef37829a0209d035aa11bebf5fcd
nguyenngocson2812 <nson09267@gmail.com>
2025-03-04 14:11:15 +0700

    Add sorting before pagination

--- src/main/java/com/sonnguyen/base/controller/UserController.java
+++ src/main/java/com/sonnguyen/base/controller/UserController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -37,9 +38,13 @@ public class UserController {
     @GetMapping
     public ResponseEntity<ApiResponse<List<User>>> getAllUsers(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDirection
     ) {
-        Pageable pageable = PageRequest.of(page, size);
+        Sort sort = sortDirection.equalsIgnoreCase("desc") ? Sort.by(sortBy).descending() : Sort.by(sortBy).ascending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+
         Page<User> userPage = userService.getUsers(pageable);
         ApiResponse<List<User>> response = new ApiResponse<>(true, "Fetched users successfully", userPage.getContent());
         return ResponseEntity.ok(response);
